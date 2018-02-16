60d5c94a8268ff096554b961d62f90917ee300df
Shyamant Achar <shyamant.achar@gmail.com>
2018-02-16 08:04:01 +0530

    reorganized ai package

---
+++ src/ai/AStarData.java
@@ -0,0 +1,12 @@
+package ai;
+
+class AStarData {
+	public Cell newCell;
+	public Cell[][] cellDetails;
+	public boolean destFound;
+	public boolean addToSet;
+	public AStarData(Cell newCell, Cell[][] cellDetails) {
+		this.newCell = newCell;
+		this.cellDetails = cellDetails;
+	}
+}
---
+++ src/ai/Cell.java
@@ -0,0 +1,28 @@
+package ai;
+
+class Cell {
+	public int parent_i, parent_j;
+	public double f, g, h;
+
+	public void setValue(int i, int j, double f, double g, double h) {
+		this.parent_i = i;
+		this.parent_j = j;
+		this.f = f;
+		this.g = g;
+		this.h = h;
+	}
+	
+	//Parameterized constructor
+	public Cell(int i, int j, double f, double g, double h) {
+		this.parent_i = i;
+		this.parent_j = j;
+		this.f = f;
+		this.g = g;
+		this.h = j;
+	}
+	
+	//Default constructor
+	public Cell() {
+	
+	}
+}
